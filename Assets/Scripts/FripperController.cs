b71bf8fc0de2c2d346da723336276d2bff55591f
高橋佑太 <yagisan555@icloud.com>
2020-07-11 22:31:48 +0900

    PinBallプロジェクト

---
+++ Assets/Scripts/FripperController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FripperController : MonoBehaviour
+{
+	private HingeJoint myHingeJoint;
+	private float defaultAngle = 20;
+	private float flickAngle = -20;
+	// Use this for initialization
+	void Start()
+	{
+		this.myHingeJoint = GetComponent<HingeJoint>();
+		SetAngle(this.defaultAngle);
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+		{
+			SetAngle(this.flickAngle);
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+		{
+			SetAngle(this.flickAngle);
+		}
+		if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+		{
+			SetAngle(this.defaultAngle);
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+		{
+			SetAngle(this.defaultAngle);
+		}
+
+
+		var touchCount = Input.touchCount;
+			
+			for (int i = 0; i < touchCount; i++)
+			{
+			Touch touch = Input.GetTouch(i);
+
+				if (touch.position.x <= Screen.width / 2)
+				{
+					if (touch.phase == TouchPhase.Began && tag == "LeftFripperTag")
+					{
+						SetAngle(this.flickAngle);
+					}
+					else if (touch.phase == TouchPhase.Ended && tag == "LeftFripperTag")
+					{
+						SetAngle(this.defaultAngle);
+					}
+				}
+				if (touch.position.x >= Screen.width / 2)
+				{
+					if (touch.phase == TouchPhase.Began && tag == "RightFripperTag")
+					{
+						SetAngle(this.flickAngle);
+					}
+					else if (touch.phase == TouchPhase.Ended && tag == "RightFripperTag")
+					{
+						SetAngle(this.defaultAngle);
+					}
+				}
+			}
+		}
+	
+	public void SetAngle(float Angle)
+	{
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = Angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+	public Text PointText;
+	private int point = 0;
+
+	// Use this for initialization
+	void Start () {
+		PointText = GameObject.Find("PointText").GetComponent<Text>();
+		PointText.text = "POINT:" + point;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void AddPoint(int score)
+    {
+		point = point + score;
+		PointText.text = "POINT:" + point;
+    }
+}
